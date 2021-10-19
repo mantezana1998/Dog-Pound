c94e1fd08bd4a24207b19314a10669869b5d1a06
mantezana <maximo00antezana@gmail.com>
2021-10-18 22:30:32 -0700

    lol

--- dogpound/settings.py
+++ dogpound/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qa=^20*c%hxjx=u-o3(#72@%qf*tjeymcj-_lizw4(^ykljc1v'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
